028c84feba30c431576463e4b218e77780c9de62
IsaAgverdiev <isamo.agverdiev@gmail.com>
2021-08-29 01:48:13 +0300

    feat: add custom UI btn+input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
+
 import './styles/App.css';
 
 function App() {
@@ -10,16 +13,27 @@ function App() {
     { id: 2, title: 'JavaScript 2', body: 'JavaScript - is programming language' },
     { id: 3, title: 'JavaScript 3', body: 'JavaScript - is programming language' },
   ]);
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'Pyton', body: 'Pyton - is programming language' },
-    { id: 2, title: 'Pyton 2', body: 'Pyton - is programming language' },
-    { id: 3, title: 'Pyton 3', body: 'Pyton - is programming language' },
-  ]);
+
+  const [title, setTitle] = useState('');
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+  };
 
   return (
     <div className='App'>
+      <from>
+        <MyInput
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type='text'
+          placeholder='post title'
+        />
+        <MyInput type='text' placeholder='post description' />
+        <MyButton onClick={addNewPost}>add post</MyButton>
+      </from>
       <PostList posts={posts} title='Posts list about JS' />
-      <PostList posts={posts2} title='Posts list about Pyton' />
     </div>
   );
 }
