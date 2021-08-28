59ba0d3d8a10cb81b1066cee562f41baaaacda0a
IsaAgverdiev <isamo.agverdiev@gmail.com>
2021-08-28 23:39:24 +0300

    feat: work with lists and map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
+import PostList from './components/PostList';
+import './styles/App.css';
 
 function App() {
-  const [value, setValue] = useState('text in input');
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'JavaScript - is programming language' },
+    { id: 2, title: 'JavaScript 2', body: 'JavaScript - is programming language' },
+    { id: 3, title: 'JavaScript 3', body: 'JavaScript - is programming language' },
+  ]);
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Pyton', body: 'Pyton - is programming language' },
+    { id: 2, title: 'Pyton 2', body: 'Pyton - is programming language' },
+    { id: 3, title: 'Pyton 3', body: 'Pyton - is programming language' },
+  ]);
 
   return (
     <div className='App'>
-      <ClassCounter />
+      <PostList posts={posts} title='Posts list about JS' />
+      <PostList posts={posts2} title='Posts list about Pyton' />
     </div>
   );
 }
