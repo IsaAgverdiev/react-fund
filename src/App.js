dd9409840077cf6fbd778cf0d828caaf20599d83
IsaAgverdiev <isamo.agverdiev@gmail.com>
2021-08-30 14:34:24 +0300

    feat: add custom hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
+import { usePosts } from './components/hooks/usePosts';
 import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
@@ -8,29 +9,12 @@ import MyModal from './components/UI/MyModal/MyModal';
 import './styles/App.css';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'php', body: ' yy php - is language' },
-    { id: 2, title: 'rubi', body: 'aa rubi - is language' },
-    { id: 3, title: 'JavaScript', body: 'pp JavaScript - is language' },
-  ]);
+  const [posts, setPosts] = useState([]);
 
-  const [filter, setFilter] = useState({
-    sort: '',
-    query: '',
-  });
+  const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
 
-  const sortedPosts = useMemo(() => {
-    console.log('отработала функция сортед постс');
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query));
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = newPost => {
     setPosts([...posts, newPost]);
--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,37 +1,38 @@
-import React, { useState } from 'react'
-import MyButton from './UI/button/MyButton'
-import MyInput from './UI/input/MyInput'
+import React, { useState } from 'react';
+import MyButton from './UI/button/MyButton';
+import MyInput from './UI/input/MyInput';
 
-const PostForm = ({create}) => {
+const PostForm = ({ create }) => {
   const [post, setPost] = useState({ title: '', body: '' });
 
-  const addNewPost = (e) => {
+  const addNewPost = e => {
     e.preventDefault();
     const newPosts = {
-      ...post, id: Date.now()
-    }
-    create(newPosts)
+      ...post,
+      id: Date.now(),
+    };
+    create(newPosts);
     setPost({ title: '', body: '' });
   };
 
   return (
-     <form>
-        {/* управляемый компонент */}
-        <MyInput
-          value={post.title}
-          onChange={(e) => setPost({ ...post, title: e.target.value })}
-          type='text'
-          placeholder='post title'
-        />
-        <MyInput
-          value={post.body}
-          onChange={(e) => setPost({ ...post, body: e.target.value })}
-          type='text'
-          placeholder='post description'
-        />
-        <MyButton onClick={addNewPost}>add post</MyButton>
-      </form>
-  )
-}
+    <form>
+      {/* управляемый компонент */}
+      <MyInput
+        value={post.title}
+        onChange={e => setPost({ ...post, title: e.target.value })}
+        type='text'
+        placeholder='Post title'
+      />
+      <MyInput
+        value={post.body}
+        onChange={e => setPost({ ...post, body: e.target.value })}
+        type='text'
+        placeholder='Post description'
+      />
+      <MyButton onClick={addNewPost}>Add</MyButton>
+    </form>
+  );
+};
 
-export default PostForm
+export default PostForm;
